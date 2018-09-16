08b482c6d326861e6232a751485cd566ee7f798d
niinoolopes <niinoolopes0@gmail.com>
2018-09-16 17:58:06 -0300

    Adiciona link para arquivo js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,50 @@
+// debounce
+
+/*
+o debounce é usado para forçar o SCROLL a exigir menos processamento do dispositivo
+assim cada SCROLL na pagina ira executar apenas 1 vez a função por cada scroll
+*/
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+// fim
+
+
+function scrollOnHeader(){
+    // obtendo "altura" da janela
+    var alturaWindow = window.pageYOffset;
+    // armazenando em variavel o elemento
+    var pegaHeader = document.querySelector('header');
+    // obtendo "HEADER" do header na variavel
+    var HeaderHeight = pegaHeader.offsetHeight;
+
+       
+    if( alturaWindow > HeaderHeight){
+        foi = "ativo";
+        pegaHeader.classList.add('ativo');
+    }else{
+        foi = "n ativo";
+        pegaHeader.classList.remove('ativo');
+    }
+    console.log(foi);
+}
+
+
+window.addEventListener('scroll', debounce(function(){
+    scrollOnHeader();
+},50));
 /*
     Nome: Anderson Lopes
     Data: 27/06/2018
@@ -32,16 +79,20 @@
     };
 /*  FIM MENU */
 /* SCROLL */
-    var itensMenu = document.querySelectorAll('.scroll');
+    var itensMenu = document.querySelectorAll('.link');
+
     itensMenu.forEach( item => {
         item.addEventListener('click', scrollToId);
     })
+
     function scrollToId(e){
         console.log("oi");
         e.preventDefault(); 
         var id = this.getAttribute('href');
-        var alturaMenuFixo = document.querySelector(".menu").offsetHeight;
+        var alturaMenuFixo = document.querySelector("header").offsetHeight;
+        console.log(alturaMenuFixo)
         var section = document.querySelector(id).offsetTop - alturaMenuFixo;
+
         window.scroll(
             {
                 top: section,
