0509f474dba283302140315416a35ad6a8d2c8ea
niinoolopes <niinoolopes0@gmail.com>
2018-09-29 09:32:34 -0300

    Adiciona Tabs ao Cronograma

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,54 +1,3 @@
-// debounce
-
-/*
-o debounce é usado para forçar o SCROLL a exigir menos processamento do dispositivo
-assim cada SCROLL na pagina ira executar apenas 1 vez a função por cada scroll
-*/
-
-const debounce = function(func, wait, immediate) {
-    let timeout;
-    return function(...args) {
-      const context = this;
-      const later = function () {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  };
-// fim
-
-
-
-
-
-function scrollOnHeader(){
-    
-    // obtendo "altura" da janela
-    var alturaWindow = window.pageYOffset;
-    // armazenando em variavel o elemento
-    var pegaHeader = document.querySelector('header');
-    // obtendo "HEADER" do header na variavel
-    var HeaderHeight = pegaHeader.offsetHeight;
-
-       
-    if( alturaWindow > HeaderHeight){ 
-        pegaHeader.classList.add('ativo');
-    }else{ 
-        pegaHeader.classList.remove('ativo');
-    } 
-} 
-
-window.addEventListener('scroll', debounce(function(){
-    scrollOnHeader(scrollToId);
-},50));
-
-window.onload =  function(event) {
-    // console.log('foi');
-}; 
 /*
     Nome: Anderson Lopes
     Data: 27/06/2018
@@ -76,14 +25,10 @@ window.onload =  function(event) {
             listMenu.classList.remove("menu-ativo");
         }
     } 
-
-
     // ativando menu com clique no botao
     btnMobile.onclick = function(){
         ShowMenu();
     };
-
-
     // desativando menu ao clicar no item do menu
     var itensMenu = document.querySelectorAll('.menu .link');
     //obtendo quantidade    
@@ -97,17 +42,52 @@ window.onload =  function(event) {
             ShowMenu();
         }
     }
-
-
-
-
-
-
-
     listMenu.onclick = function(){
         listMenu.classList.remove("active");
     };
 /*  FIM MENU */
+// debounce
+
+/*
+o debounce é usado para forçar o SCROLL a exigir menos processamento do dispositivo
+assim cada SCROLL na pagina ira executar apenas 1 vez a função por cada scroll
+*/
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+// fim
+
+
+
+function scrollOnHeader(){
+    // obtendo "altura" da janela
+    var alturaWindow = window.pageYOffset;
+    // armazenando em variavel o elemento
+    var pegaHeader = document.querySelector('header');
+    // obtendo "HEADER" do header na variavel
+    var HeaderHeight = pegaHeader.offsetHeight;
+
+    if( alturaWindow > HeaderHeight){ 
+        pegaHeader.classList.add('ativo');
+    }else{ 
+        pegaHeader.classList.remove('ativo');
+    } 
+} 
+
+window.addEventListener('scroll', debounce(function(){
+    scrollOnHeader(scrollToId);
+},50));
 /* SCROLL */
     var itensMenu = document.querySelectorAll('.link');
 
@@ -148,3 +128,112 @@ window.onload =  function(event) {
         ); 
     }
 /* FIM SCROLL */
+/*
+// ativando função para resize do navegador 
+window. addEventListener("resize", function(){ tabInWorks() });
+// ativando função ao carregando completo do DOM
+window. addEventListener("load", function(){ tabInWorks() }); 
+
+
+function tabInWorks(){
+  var width = window.innerWidth;
+  if(width<1040){
+      document.querySelector('.row.tab').style.display = "flex";
+      document.querySelector('#dia1').style.display = "block";
+      document.querySelector('#dia2').style.display = "none";
+      document.querySelector('#dia3').style.display = "none";
+      
+
+      //for(i=0;){
+      //}
+
+      // console.log('tab ativa sim')
+  }else{
+      document.querySelector('.row.tab').style.display = "none";
+      document.querySelector('#dia1').style.display = "block";
+      document.querySelector('#dia2').style.display = "block";
+      document.querySelector('#dia3').style.display = "block";
+      // console.log('tab desativada')
+  }
+} 
+
+*/
+var qnt = document.querySelectorAll('.fade');
+for(i=1; i<qnt.length; i++){
+  qnt[i].style.display = "none";
+  console.log('');
+}
+ 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  /*
+  var dots = document.getElementsByClassName("dot");
+  */
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  /*
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  */
+  slides[slideIndex-1].style.display = "block";  
+  /*
+  dots[slideIndex-1].className += " active";
+  */
+} 
+// ativando função para resize do navegador 
+window. addEventListener("resize", function(){ tabInWorks() });
+// ativando função ao carregando completo do DOM
+window. addEventListener("load", function(){ tabInWorks() }); 
+
+function tabInWorks(){
+    var width = window.innerWidth;
+    if(width<1040){
+        document.querySelector('.row.tab').style.display = "flex";
+        document.querySelector('#dia1').style.display = "block";
+        document.querySelector('#dia2').style.display = "none";
+        document.querySelector('#dia3').style.display = "none";
+        // console.log('tab ativa sim')
+    }else{
+        document.querySelector('.row.tab').style.display = "none";
+        document.querySelector('#dia1').style.display = "block";
+        document.querySelector('#dia2').style.display = "block";
+        document.querySelector('#dia3').style.display = "block";
+        // console.log('tab desativada')
+    }
+} 
+
+
+// função
+function openCity(evt, diaCronograma) { 
+    var i, tabcontent, tablinks; 
+    tabcontent = document.getElementsByClassName("tab_card");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+/*
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+*/
+    document.getElementById(diaCronograma).style.display = "block";
+    evt.currentTarget.className += " active"; 
+    console.log('tab '+diaCronograma+' ativa')
+}
